d3731941374e302d9820d599abced150c8a90877
Asus <turgunboyevjurabek777@gmail.com>
2023-12-12 20:41:51 +0500

    somi ui update

--- app/src/main/java/uz/turgunboyevjurabek/saxovat/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/saxovat/MainActivity.kt
@@ -2,11 +2,19 @@ package uz.turgunboyevjurabek.saxovat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import uz.turgunboyevjurabek.saxovat.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
 
     }
 }
