6a76e9b42053818a31bf13786e50e5c94c3bb5c8
Asus <turgunboyevjurabek777@gmail.com>
2023-12-13 18:56:57 +0500

    login end code fragment eding and code starting ...

---
+++ app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/homeFragment/HomeFragment.kt
@@ -0,0 +1,31 @@
+package uz.turgunboyevjurabek.saxovat.ui.fragments.homeFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import uz.turgunboyevjurabek.saxovat.R
+import uz.turgunboyevjurabek.saxovat.databinding.FragmentHomeBinding
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.loginFragment)
+    }
+}
--- app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/CodeFragment.kt
+++ app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/CodeFragment.kt
@@ -1,24 +1,23 @@
-package uz.turgunboyevjurabek.saxovat.ui.fragments
+package uz.turgunboyevjurabek.saxovat.ui.fragments.loginFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import uz.turgunboyevjurabek.saxovat.R
-import uz.turgunboyevjurabek.saxovat.databinding.FragmentHomeBinding
+import uz.turgunboyevjurabek.saxovat.databinding.FragmentCodeBinding
 
-@AndroidEntryPoint
-class HomeFragment : Fragment() {
-    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+class CodeFragment : Fragment() {
+    private val binding by lazy { FragmentCodeBinding.inflate(layoutInflater)}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
 
+        binding.pinView.setAnimationEnable(true)
+        binding.pinView.isPasswordHidden=true
 
         return binding.root
     }
