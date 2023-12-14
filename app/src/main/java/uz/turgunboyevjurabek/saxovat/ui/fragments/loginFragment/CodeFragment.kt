7c46fbd462620d86f81bda241bf0f91c7493a9a6
Asus <turgunboyevjurabek777@gmail.com>
2023-12-14 12:13:59 +0500

    update ui

--- app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/CodeFragment.kt
+++ app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/CodeFragment.kt
@@ -1,10 +1,13 @@
 package uz.turgunboyevjurabek.saxovat.ui.fragments.loginFragment
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import uz.turgunboyevjurabek.saxovat.R
 import uz.turgunboyevjurabek.saxovat.databinding.FragmentCodeBinding
 
@@ -16,9 +19,35 @@ class CodeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+
+
+        return binding.root
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         binding.pinView.setAnimationEnable(true)
         binding.pinView.isPasswordHidden=true
 
-        return binding.root
+        edtWork()
+
+
+
+    }
+
+    private fun edtWork() {
+
+        binding.pinView.addTextChangedListener {
+
+            if (binding.pinView.text.isNullOrEmpty()){
+                binding.btnContinue.setBackgroundColor(resources.getColor(R.color.btn_false))
+                binding.pinView.setLineColor(resources.getColor(R.color.btn_false))
+            }else{
+                binding.btnContinue.setBackgroundColor(resources.getColor(R.color.btn_true))
+                binding.pinView.setLineColor(resources.getColor(R.color.btn_true))
+            }
+        }
     }
 }
--- app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/LoginFragment.kt
+++ app/src/main/java/uz/turgunboyevjurabek/saxovat/ui/fragments/loginFragment/LoginFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import uz.turgunboyevjurabek.saxovat.R
+import uz.turgunboyevjurabek.saxovat.databinding.ChackNumberDialogBinding
 import uz.turgunboyevjurabek.saxovat.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -23,9 +27,53 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        edtWork()
+
+       checkNumberDialog()
+
+
+    }
+
+    private fun checkNumberDialog() {
+        val checkNumberDialogBinding=ChackNumberDialogBinding.inflate(layoutInflater)
+        val dialog =MaterialAlertDialogBuilder(requireContext()).create()
+        dialog.setCancelable(false)
+        dialog.setView(checkNumberDialogBinding.root)
+
+
 
         binding.btnContinue.setOnClickListener {
-            findNavController().navigate(R.id.codeFragment)
+            binding.mainLayout.alpha=0.1f
+            dialog.show()
+            checkNumberDialogBinding.btnYes.setOnClickListener {
+                findNavController().navigate(R.id.codeFragment)
+                dialog.cancel()
+                binding.mainLayout.alpha=1f
+            }
+            checkNumberDialogBinding.btnNo.setOnClickListener {
+                dialog.cancel()
+                binding.mainLayout.alpha=1f
+            }
+            var number=binding.edtMasked.unMaskedText
+
+            checkNumberDialogBinding.thtNumber.text=number.toString().trim()
+            checkNumberDialogBinding.divider.dividerInsetEnd=number?.length!!
+        }
+
+
+    }
+
+    private fun edtWork() {
+
+        binding.edtMasked.addTextChangedListener {
+
+            if (binding.edtMasked.text.isNullOrEmpty()){
+                binding.btnContinue.setBackgroundColor(resources.getColor(R.color.btn_false))
+                binding.edtMasked.setBackgroundResource(R.drawable.shape_edt_false)
+            }else{
+                binding.btnContinue.setBackgroundColor(resources.getColor(R.color.btn_true))
+                binding.edtMasked.setBackgroundResource(R.drawable.shape_edt_true)
+            }
         }
     }
 }
